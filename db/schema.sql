2347da0fc320cb3e0247bf80eba2a90d7cabce43
Brandon King <brandonscottking@gmail.com>
2021-05-09 17:00:22 -0400

    updated file structure; wrote server.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,23 @@
 DROP DATABASE IF EXISTS blog_db;
 
 CREATE DATABASE blog_db;
+
+/*
+Posts
+    ID
+    title
+    contents
+    author *user.ID
+    date
+    comments *comment.ID
+Users
+    ID
+    username
+    password
+Comments
+    ID
+    author *user.ID
+    post *blog.ID
+    content
+    date
+*/
--- server.js
+++ server.js
@@ -9,3 +9,29 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'My Super secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+});
