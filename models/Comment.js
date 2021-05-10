42620da886bb41a1e8a5d0aebc0811a6b0b0772e
Brandon King <brandonscottking@gmail.com>
2021-05-10 16:30:24 -0400

    started comment model

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+const { join } = require('bluebird');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    
+}   
+)
