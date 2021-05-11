f47ecd3605400f1a047100b7160da6539e962507
Brandon King <brandonscottking@gmail.com>
2021-05-10 22:48:35 -0400

    finished Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -11,6 +11,33 @@ Comment.init({
         primaryKey: true,
         autoIncrement: true,
     },
-    
-}   
-)
+    comment: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id',
+        }
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+}
+); 
+
+module.exports = Comment;
