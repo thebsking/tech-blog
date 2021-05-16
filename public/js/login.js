2616e76a08421cf4b9c4de6646bdb5f8456f7d7a
Brandon King <brandonscottking@gmail.com>
2021-05-15 21:40:38 -0400

    working on "signup form" modal

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,15 @@
+const createButton = document.querySelector('#create-btn');
+
 const loginHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#username')
-    const password = document.querySelector('#password')
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
 
-    if(email && password) {
+    if(username && password) {
         const response = await fetch ('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({email, password}),
+            body: JSON.stringify({username, password}),
             headers: { 'Content-Type': 'applications/json' },
         });
 
@@ -17,4 +19,15 @@ const loginHandler = async (event) => {
             alert(response.statusText)
         }
     }
+};
+
+const signupHandler = async (event) => {
+    event.preventDefault();
+
+    const nameEl = document.querySelector('#signup-name');
+    const userEl = document.querySelector('#signup-username');
+    const passEl = document.querySelector('#signup-password');
 }
+
+document.querySelector('#login-form').addEventListener('submit', loginHandler)
+document.querySelector('#signup-form')
